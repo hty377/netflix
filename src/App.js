09694feb9ec463a7d2c20f08d598f029865aa2db
hty377 <33384830+hty377@users.noreply.github.com>
2020-03-10 00:04:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {getMyList, getRecom} from './http/httpInstance'
 import { connect } from "react-redux";
 import Mod from './pages/mod'
 import imgURL from './pic/logo.png'
-//import Bottom from './pages/bottom'
   
 class App extends React.Component {
     constructor (props) {
@@ -27,6 +26,7 @@ class App extends React.Component {
         )
     }
   
+   //lifecycle method, render the component, load the data
     async componentDidMount () {
       let list = await getMyList()
       let recom = await getRecom()
@@ -36,32 +36,39 @@ class App extends React.Component {
    
 }
 
+// map the data in the store to the component's props
 function mapStateToProps (state) {
   return {
     listData: state.listData,
     recomData: state.recomData
   }
 }
+
+//map the dispatch to the components props
 function mapDispatchToProps (dispatch) {
   return {
+    // update the data in the myList to the state
     updateListEvent: (data) => {
       dispatch({
         type:'updateList',
         data:data
       })
     },
+    // update the data in the recommendation to the state
     updateRecomEvent:(data) => {
       dispatch({
         type:'updateRecom',
         data:data
       })
     },
+    //after click the 'remove' button, the data in myList -1, and data in recomendaton + 1
     delListEvent: (id) => {
       dispatch({
         type: 'delList',
         id:id
       })
     },
+    //after click the 'add' button, the data in recommendation -1, and data in myList +1
     delRecomEvent:(id) => {
       dispatch({
         type: 'delRecom',
@@ -71,6 +78,7 @@ function mapDispatchToProps (dispatch) {
   }
 }
 
+// connect this two funcs to the component
 export default connect (
   mapStateToProps,
   mapDispatchToProps
