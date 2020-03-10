3d4270fe1ca76457ee959199e7ccaf3fa259eb91
hty377 <33384830+hty377@users.noreply.github.com>
2020-03-10 00:00:01 -0400

    Update http.js

--- src/http/http.js
+++ src/http/http.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
-// 环境的判断
+// 
 
 // axios.defaults.baseURL = 'http://49.233.184.116:5001/'
 
 
-// 设置请求超时
+//request timeout 
 axios.defaults.timeout = 10000
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
 
 /**
- * get方法，对应get请求
- * @param {String} url [请求的url地址]
- * @param {Object} params [请求时携带的参数]
+ * get method
+ * @param {String} url [request url address]
+ * @param {Object} params [parameters]
  */
 export function get(url, params = {}){    
     return new Promise((resolve, reject) =>{        
@@ -24,22 +24,3 @@ export function get(url, params = {}){
             reject(err.data)        
     })
 });}
-
-/** 
- * post方法，对应post请求 
- * @param {String} url [请求的url地址] 
- * @param {Object} params [请求时携带的参数] 
- */
-export function post(url, params) {
-    return new Promise((resolve, reject) => {
-        axios.post( url, params)
-        .then(res => {
-            resolve(res.data);
-        })
-        .catch(err =>{
-            reject(err.data)
-        })
-    });
-}
-
-
